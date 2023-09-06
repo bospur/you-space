b61dbf78736e6133cbd679f2d171a531aa7af913
Ivan <i.semenov@eputs.org>
2023-09-06 15:56:53 +0300

    Add purchases form

---
+++ src/componets/Purchases/Purchases.tsx
@@ -0,0 +1,26 @@
+import { Button, Form, Input, Select } from "antd";
+import styles from "./Purchases.module.scss";
+import { getСategoryOptions } from "./helpers/getСategoryOptions";
+import { useForm } from "antd/es/form/Form";
+
+const Purchases = () => {
+  const [form] = useForm();
+  return (
+    <div className={styles.wrapper}>
+      <h2>Добавить покупку</h2>
+      <Form form={form} layout="vertical" size="large">
+        <Form.Item label="Категория" name="category">
+          <Select options={getСategoryOptions()} />
+        </Form.Item>
+        <Form.Item label="Сумма" name="amount">
+          <Input />
+        </Form.Item>
+      </Form>
+      <Button type="primary" size="large">
+        Отправить
+      </Button>
+    </div>
+  );
+};
+
+export default Purchases;
---
+++ src/componets/Purchases/helpers/getСategoryOptions.ts
@@ -0,0 +1,48 @@
+export const getСategoryOptions = () => {
+  return [
+    {
+      label: "Аренда жилья",
+      value: "rent",
+    },
+    {
+      label: "Питание",
+      value: "nutrition",
+    },
+    {
+      label: "Здоровье",
+      value: "health",
+    },
+    {
+      label: "Животные",
+      value: "animals",
+    },
+    {
+      label: "Бытовые расходы",
+      value: "household",
+    },
+    {
+      label: "Одежда",
+      value: "clothes",
+    },
+    {
+      label: "Спорт",
+      value: "sport",
+    },
+    {
+      label: "Развлечения",
+      value: "entertainments",
+    },
+    {
+      label: "Проезд",
+      value: "travel",
+    },
+    {
+      label: "Копилка",
+      value: "bank",
+    },
+    {
+      label: "НПЗ",
+      value: "surprise",
+    },
+  ];
+};
